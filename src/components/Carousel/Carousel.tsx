ae5a11da392f43d1087ef659d93c9904f3053133
Shubhankar <shubhankarvalimbe@gmail.com>
2026-07-10 05:40:09 -0400

    Create full-bleed peeking carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,23 +1,33 @@
 'use client';
 
-import React from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 
 export default function EmblaCarousel() {
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'start', containScroll: 'trimSnaps' });
+  const slides = Array.from({ length: 5 }, (_, index) => `Slide ${index + 1}`);
+  const scrollPrev = () => emblaApi?.scrollPrev();
+  const scrollNext = () => emblaApi?.scrollNext();
 
   return (
     <div className="embla">
-      <div className="embla__viewport overflow-hidden" ref={emblaRef}>
-        <div className="embla__container flex touch-pan-y touch-pinch-zoom">
-          <div className="embla__slide">Slide 1</div>
-          <div className="embla__slide">Slide 2</div>
-          <div className="embla__slide">Slide 3</div>
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((slide) => (
+            <div className="embla__slide border" key={slide}>
+              {slide}
+            </div>
+          ))}
         </div>
       </div>
 
-      <button className="embla__prev">Scroll to prev</button>
-      <button className="embla__next">Scroll to next</button>
+      <div className="mt-10 flex justify-center space-x-2">
+        <button className="embla__prev" onClick={scrollPrev}>
+          Scroll to prev
+        </button>
+        <button className="embla__next" onClick={scrollNext}>
+          Scroll to next
+        </button>
+      </div>
     </div>
   );
 }
