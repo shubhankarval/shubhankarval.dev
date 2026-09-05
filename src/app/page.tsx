36b9071e8d7064b863858827dd857d160aa7ce7d
Shubhankar <59073366+shubhankarval@users.noreply.github.com>
2026-09-04 23:43:51 -0400

    Empty main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Carousel from '@components/Carousel/Carousel';
-
 export default async function Home() {
-  return (
-    <div className="min-h-screen w-full">
-      <Carousel />
-    </div>
-  );
+  return <div></div>;
 }
